8af662cbf96f736ed7b37e85b4018cb366716443
Nishant Mehta <nishme0708@gmail.com>
2023-04-02 14:52:29 +0530

    Add code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+import data from './data';
+import List from './List';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  const handleClear = ()=>{
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people}></List>
+        <button className="btn btn-block" onClick={handleClear}>Clear All</button>
+      </section>
+    </main>
+  );
 };
 export default App;
